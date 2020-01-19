c13f1b7146ab9897aa8735f0c7474065976a61fa
Angelo Guerrero <amcgdev@gmail.com>
2020-01-19 09:41:58 +0400

    - Set up knex

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: __dirname + '/src/database/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/src/database/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/database/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
